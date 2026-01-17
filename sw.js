b0af843c35aba0d531d4723adbc059a3bab2e334
dark-entity254 <emumo363@gmail.com>
2026-01-17 16:01:47 +0300

    Implement push notifications in sw.js
    Add push notification support in service worker

--- sw.js
+++ sw.js
@@ -12,3 +12,13 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: 'icon.png',
+        badge: 'icon.png',
+        vibrate: [200, 100, 200]
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+});
